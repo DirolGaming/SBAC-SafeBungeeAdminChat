53294cc32a14dcc3e19e2d85842d3b36d31e2147
Jaagop Janson <DirolGaming@gmail.com>
2017-12-10 00:49:18 +0200

    Optmized and fixed file management

--- src/main/java/me/dirolgaming/safebac/Main.java
+++ src/main/java/me/dirolgaming/safebac/Main.java
@@ -6,7 +6,12 @@ import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -33,35 +38,55 @@ public final class Main extends Plugin {
         getProxy().getPluginManager().registerListener(this, new Listener_CHAT(this));
         getProxy().getPluginManager().registerCommand(this, new Command_SAFEBAC(this));
         getLogger().info("SafeBAC version " + this.getDescription().getVersion() + " has been enabled.");
-
-        getProxy().getScheduler().runAsync(this, () ->
-            new MetricsLite(this)
-        );
+        getProxy().getScheduler().runAsync(this, () -> new MetricsLite(this));
+        checkUpdate();
     }
 
-    private void setupConfig() {
-        try {
-            if(Files.notExists(configurationFile.getParent()))
-                Files.createDirectories(configurationFile.getParent());
+    private void checkUpdate() {
+        if (getConfig().getBoolean("check-update")) {
+            try {
+                HttpURLConnection con = (HttpURLConnection) (new URL("http://www.spigotmc.org/api/general.php")).openConnection();
+                con.setDoOutput(true);
+                con.setRequestMethod("POST");
+                con.getOutputStream().write("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=50530".getBytes("UTF-8"));
+                String version = (new BufferedReader(new InputStreamReader(con.getInputStream()))).readLine();
+                con.disconnect();
+                if (!version.equals(getDescription().getVersion())) {
+                    getLogger().warning("There is an update available for SafeBAC. Please use the latest version at all times.");
+                    getLogger().warning("Your version: " + getDescription().getVersion() + " | " + "New version: " + version);
+                    getLogger().warning("Download it here: https://www.spigotmc.org/resources/sbac-safebungeeadminchat.50530/");
+                }
+            } catch (Exception ignored) {
+            }
+        }
+    }
 
-            if (!Files.notExists(configurationFile))
-                Files.copy(this.getResourceAsStream("config.yml"), configurationFile);
+    public void setupConfig() {
+        try {
+            if (!this.getDataFolder().exists()) {
+                this.getDataFolder().mkdir();
+            }
+            File file = new File(this.getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                Files.copy(this.getResourceAsStream("config.yml"), file.toPath());
+            }
+            loadConfig();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void loadConfig() {
+    public void loadConfig() {
         try {
-            this.config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configurationFile.toFile());
+            this.config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(this.getDataFolder(), "config.yml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void saveConfig() {
+    public void saveConfig() {
         try {
-            ConfigurationProvider.getProvider(YamlConfiguration.class).save(getConfig(), configurationFile.toFile());
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(getConfig(), new File(this.getDataFolder(), "config.yml"));
         } catch (IOException e) {
             e.printStackTrace();
         }

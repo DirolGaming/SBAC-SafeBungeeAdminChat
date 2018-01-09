ce214b4dc47edbabb11a0ae69dc43cff3a5062b6
Jaagop Janson <DirolGaming@gmail.com>
2018-01-10 00:20:18 +0200

    Added helpopmsg executor

--- src/main/java/me/dirolgaming/safebac/Utils.java
+++ src/main/java/me/dirolgaming/safebac/Utils.java
@@ -5,6 +5,7 @@ import me.lucko.luckperms.api.Contexts;
 import me.lucko.luckperms.api.LuckPermsApi;
 import me.lucko.luckperms.api.User;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
@@ -18,32 +19,37 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
  */
 final class Utils {
     public final LuckPermsApi api = LuckPerms.getApi();
+
     static String buildMessage(String[] args) {
         StringBuilder acb = new StringBuilder();
-        for(String arg : args)
+        for (String arg : args)
             acb.append(arg).append(" ");
         return acb.toString();
     }
-        static String getPrefix(ProxiedPlayer p) {
-            if(ProxyServer.getInstance().getPluginManager().getPlugin("LuckPerms") == null) {
-                return "";
-            }
-        final LuckPermsApi lpAPI = LuckPerms.getApi();
-            User user = lpAPI.getUser(p.getUniqueId());
-            Contexts userCtx = lpAPI.getContextForUser(user).orElseThrow(() -> new IllegalStateException("Could not get LuckPerms context for player " + p));
-            return user.getCachedData().getMetaData(userCtx).getPrefix();
+
+    static String getPrefix(ProxiedPlayer p) {
+        if (ProxyServer.getInstance().getPluginManager().getPlugin("LuckPerms") == null) {
+            return "";
         }
+        final LuckPermsApi lpAPI = LuckPerms.getApi();
+        User user = lpAPI.getUser(p.getUniqueId());
+        Contexts userCtx = lpAPI.getContextForUser(user).orElseThrow(() -> new IllegalStateException("Could not get LuckPerms context for player " + p));
+        return user.getCachedData().getMetaData(userCtx).getPrefix();
+    }
+
     static void broadcastAdminchatMessage(Main safeBAC, ProxiedPlayer p, String message) {
         TextComponent evac = new TextComponent(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', safeBAC.getConfig().getString("adminchat-format")
                 .replace("%player%", p.getName())
                 .replace("%prefix%", getPrefix(p))
+                .replace("%server%", p.getServer().getInfo().getName())
                 .replace("%message%", message))));
 
         if (safeBAC.getConfig().getBoolean("adminchat-hover.enable")) {
             evac.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', safeBAC.getConfig().getString("adminchat-hover.message")
                     .replace("%server%", p.getServer().getInfo().getName())
                     .replace("%prefix%", getPrefix(p))
-                    )).create()));
+                    .replace("%server%", p.getServer().getInfo().getName())
+            )).create()));
         }
 
         if (safeBAC.getConfig().getBoolean("adminchat-click.enable")) {
@@ -64,5 +70,61 @@ final class Utils {
                 ev.sendMessage(evac);
             }
         }
+    }
+
+    static void broadcasthelpopmsg(Main cac, ProxiedPlayer p, CommandSender s, String message) {
+        TextComponent evhp = new TextComponent(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', cac.getConfig().getString("helpop-received")
+                .replace("%player%", p.getName())
+                .replace("%prefix%", Utils.getPrefix(p))
+                .replace("%server%", p.getServer().getInfo().getName())
+                .replace("%message%", message))));
+        TextComponent evac = new TextComponent(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', cac.getConfig().getString("helpop-format")
+                .replace("%player%", p.getName())
+                .replace("%prefix%", Utils.getPrefix(p))
+                .replace("%server%", p.getServer().getInfo().getName())
+                .replace("%message%", message))));
+
+        if (cac.getConfig().getBoolean("helpop-hover.enable")) {
+            evac.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', cac.getConfig().getString("helpop-hover.message")
+                    .replace("%prefix%", Utils.getPrefix(p))
+                    .replace("%server%", p.getServer().getInfo().getName()))).create()));
+        }
+
+        if (cac.getConfig().getBoolean("helpop-click.enable")) {
+            if (cac.getConfig().getBoolean("helpop-click.url.enable")) {
+                evac.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, cac.getConfig().getString("helpop-click.url.url")));
+            }
+
+            if (cac.getConfig().getBoolean("helpop-click.command.enable")) {
+                evac.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, cac.getConfig().getString("helpop-click.command.command")
+                        .replace("%prefix%", Utils.getPrefix(p))
+                        .replace("%server%", p.getServer().getInfo().getName())));
+            }
+        }
+
+        if (cac.getConfig().getBoolean("helpop-received-hover.enable")) {
+            evhp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', cac.getConfig().getString("helpop-received-hover.message")
+                    .replace("%prefix%", Utils.getPrefix(p))
+                    .replace("%server%", p.getServer().getInfo().getName()))).create()));
+        }
+
+        if (cac.getConfig().getBoolean("helpop-received-click.enable")) {
+            if (cac.getConfig().getBoolean("helpop-received-click.url.url")) {
+                evac.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, cac.getConfig().getString("helpop-received-click.url.url")));
+            }
+
+            if (cac.getConfig().getBoolean("helpop-received-click.command.enable")) {
+                evac.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, cac.getConfig().getString("helpop-received-click.command.command")
+                        .replace("%prefix%", Utils.getPrefix(p))
+                        .replace("%server%", p.getServer().getInfo().getName())));
+            }
+        }
+
+        for (ProxiedPlayer ev : cac.getProxy().getPlayers()) {
+            if (ev.hasPermission("sbac.helpop.receive")) {
+                ev.sendMessage(evac);
+            }
+        }
+        s.sendMessage(evhp);
     }
 }

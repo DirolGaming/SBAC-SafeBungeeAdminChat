cfb2f79b8106a0039994e74b6495f2f74ac14ea3
Jaagop Janson <DirolGaming@gmail.com>
2018-01-09 18:26:34 +0200

    Added Prefix placeholder

--- src/main/java/me/dirolgaming/safebac/Utils.java
+++ src/main/java/me/dirolgaming/safebac/Utils.java
@@ -1,17 +1,22 @@
 package me.dirolgaming.safebac;
 
+import me.lucko.luckperms.LuckPerms;
+import me.lucko.luckperms.api.Contexts;
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+
 /**
  * @author Mark Vainomaa
  */
 final class Utils {
+    public final LuckPermsApi api = LuckPerms.getApi();
     static String buildMessage(String[] args) {
         StringBuilder acb = new StringBuilder();
         for(String arg : args)
@@ -19,9 +24,16 @@ final class Utils {
         return acb.toString();
     }
 
+        static String getPrefix(ProxiedPlayer p) {
+        final LuckPermsApi lpAPI = LuckPerms.getApi();
+            User user = lpAPI.getUser(p.getUniqueId());
+            Contexts userCtx = lpAPI.getContextForUser(user).orElseThrow(() -> new IllegalStateException("Could not get LuckPerms context for player " + p));
+            return user.getCachedData().getMetaData(userCtx).getPrefix();
+        }
     static void broadcastAdminchatMessage(Main safeBAC, ProxiedPlayer p, String message) {
         TextComponent evac = new TextComponent(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', safeBAC.getConfig().getString("adminchat-format")
                 .replace("%player%", p.getName())
+                .replace("%prefix%", getPrefix(p))
                 .replace("%message%", message))));
 
         if (safeBAC.getConfig().getBoolean("adminchat-hover.enable")) {
